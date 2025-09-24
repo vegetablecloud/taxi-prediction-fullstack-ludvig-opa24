1db8cb33f2b7a9e1b0022d1c94be034aec19ca8a
vegetablecloud <ludvig.johansson@hotmail.com>
2025-09-24 15:28:22 +0200

    FEAT: Create 'predict endpoint' that convert user input data to ml dataframe format features

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -2,4 +2,9 @@ from importlib.resources import files
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
 
+TAXI_MODEL_PATH = files("taxipred").joinpath("models/broad_responsible_xgb_model.joblib")
+
 # DATA_PATH = Path(__file__).parents[1] / "data"
+
+if __name__ == "__main__":
+    print(TAXI_MODEL_PATH)
