1bbefaddf8e3d17b273d435768b860cdcfbf40b4
vegetablecloud <ludvig.johansson@hotmail.com>
2025-09-25 14:41:46 +0200

    Add googlemaps api for distance and duration conversion of userinput

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -2,23 +2,25 @@ from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
 from pydantic import BaseModel, Field
 from datetime import datetime
-from taxipred.backend import models
 import joblib
-from taxipred.utils.constants import TAXI_MODEL_PATH
 import xgboost as xgb
 import pandas as pd
 from pathlib import Path
+import googlemaps
+from googlemaps.directions import directions
+import os
+from dotenv import load_dotenv
+import json
+load_dotenv()
 
 
 TAXI_ML_PATH = Path(__file__).parents[0] / "models"
 
-class TripInput(BaseModel):
-    distance: float
-    passenger_count: int = Field(gt=0, lt=5)
-    weather: str
-    duration_minutes: float
+class UserInput(BaseModel):
+    pickup: str
+    destination: str
+    nr_passengers: int = Field(gt=0, lt=5)
     date: datetime
-
 
 app = FastAPI()
 
@@ -29,43 +31,84 @@ async def read_taxi_data():
     return taxi_data.to_json()
 
 
-@app.post("/predict")
-def predict_taxi_price(input: TripInput):
+def retrieve_google_direction_results(input_data: UserInput):
+    """
+    Takes in a validated UserInput pydantic instance object, and sends its data to google maps API and return its results
+    """
+    api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    gmaps = googlemaps.Client(key=api_key)
     
-    dict_values = input.model_dump()
+    pickup = input_data.pickup
+    destination = input_data.destination
+    departure_time = input_data.date
 
-    # Logic here to convert the user inputs to be able to to be converted so it matches the feats
-    """
-	Trip_Distance_km	Passenger_Count	Trip_Duration_Minutes	Time_of_Day_Evening	Time_of_Day_Morning	Time_of_Day_Night	Time_of_Day_Unknown	Day_of_Week_Weekday	Day_of_Week_Weekend	Weather_Rain	Weather_Snow	Weather_Unknown
-    0	19.35	3.0	53.82	False	True	False	False	True	False	False	False	False
-    """
-    # Here we need to map the values. We need this row finnsihed perhaps..
+    direction_results = directions(
+        client=gmaps,
+        origin=pickup,
+        destination=destination,
+        mode="driving",
+        departure_time=departure_time
+    )
 
-    dict_for_model = {
-        "Trip_Distance_km": dict_values["distance"],
-        "Passenger_Count": dict_values["passenger_count"],
-        "Trip_Duration_Minutes": dict_values["duration_minutes"],
-        "Time_of_Day_Evening": True if dict_values["date"].hour >= 18 and dict_values["date"].hour < 24 else False,
-        "Time_of_Day_Morning": True if dict_values["date"].hour >= 6 and dict_values["date"].hour < 12 else False,
-        "Time_of_Day_Night": True if dict_values["date"].hour >= 0 and dict_values["date"].hour < 6 else False,
-        "Time_of_Day_Unknown": True if dict_values["date"].hour < 0 else False,
-        "Day_of_Week_Weekday": True if dict_values["date"].weekday() < 5 else False,
-        "Day_of_Week_Weekend": True if dict_values["date"].weekday() >= 5 else False,
-        "Weather_Rain": True if dict_values["weather"] == "Rain" else False,
-        "Weather_Snow": True if dict_values["weather"] == "Snow" else False,
-        "Weather_Unknown": True if dict_values["weather"] not in ["Rain", "Snow", "Clear"] else False
+    return direction_results[0]["legs"][0]
+
+
+def prepare_features(input_data: UserInput) -> pd.DataFrame:
+    """
+    Retrives user input in a pydantic model and convertes it to a dataframe our Ml-model can understand.
+    """
+    data = input_data.model_dump()
+
+    trip_info = retrieve_google_direction_results(input_data=input_data)
+
+
+
+    feature_dict = {
+        "Trip_Distance_km": trip_info["distance"]["value"] / 1000,  # API Google will deliver this
+        "Passenger_Count": data["passenger_count"],
+        "Trip_Duration_Minutes": trip_info["duration"]["value"] / 60,  # API Google will deliver this
+        "Time_of_Day_Evening": True if data["date"].hour >= 18 and data["date"].hour < 24 else False,
+        "Time_of_Day_Morning": True if data["date"].hour >= 6 and data["date"].hour < 12 else False,
+        "Time_of_Day_Night": True if data["date"].hour >= 0 and data["date"].hour < 6 else False,
+        "Time_of_Day_Unknown": True if data["date"].hour < 0 else False,
+        "Day_of_Week_Weekday": True if data["date"].weekday() < 5 else False,
+        "Day_of_Week_Weekend": True if data["date"].weekday() >= 5 else False,
+        "Weather_Rain": True if data["weather"].lower() == "rain" else False,                   # Use weather varible that fetches the weather
+        "Weather_Snow": True if data["weather"].lower() == "snow" else False,                   # Use weather varible that fetches the weather
+        "Weather_Unknown": True if data["weather"] not in ["Rain", "Snow", "Clear"] else False  # Use weather varible that fetches the weather
     }
 
-    # Convert dict_for_model to dataframe
-    df_input_values = pd.DataFrame([dict_for_model])
-    
-    print(df_input_values)
+    df = pd.DataFrame([feature_dict])
 
+    return df
+
+@app.post("/predict")
+def predict_taxi_price(input: UserInput):
+    
+    
+    # Retrived the trained XGB model. 
     model: xgb.XGBRegressor = joblib.load(TAXI_ML_PATH / "broad_responsible_xgb_model.joblib")
     
-    y_pred = model.predict(df_input_values)
+    # Predict with the user input values
+    #y_pred = model.predict(df_input_values)
 
-    return f" Din resa kostar uppskattningsvis {y_pred[0]:.2f} dollar!"
+    # Return the predicted value
+    #return y_pred[0]
     
 
 
+if __name__ == "__main__":
+    # Example and test of the function
+    user_input = UserInput(
+        pickup="Dunderbergsvägen 2 sjömarken",
+        destination="Värmlandsgatan 2 Göteborg",
+        nr_passengers=2,
+        # date should be during rushhour for better testing wedsnesday 8am 2025
+        date=datetime(2025, 10, 1, 8, 0, 0)
+    )
+
+    result = retrieve_google_direction_results(user_input)
+    result
+    # dump into json file
+    with open("direction_results.json", "w") as f:
+        json.dump(result, f, indent=4)
