439679779bbfec7675771ff1fd6adcd75edf08de
vegetablecloud <ludvig.johansson@hotmail.com>
2025-10-01 14:17:35 +0200

    Feat: added a enviorment varibel for url

--- src/taxipred/utils/helpers.py
+++ src/taxipred/utils/helpers.py
@@ -1,16 +1,16 @@
-import requests 
+import requests
+import os 
 from urllib.parse import urljoin
 
-def read_api_endpoint(endpoint = "/", base_url = "http://api:8000"):
+API_BASE_URL = os.getenv("API_URL", "http://api:8000") 
+
+def read_api_endpoint(endpoint = "/", base_url = API_BASE_URL):
     url = urljoin(base_url, endpoint)
     response = requests.get(url)
     
     return response
 
-def post_api_endpoint(endpoint = "/", base_url = "http://api:8000", json = None):
+def post_api_endpoint(endpoint = "/", base_url = API_BASE_URL, json = None):
     url = urljoin(base_url, endpoint)
     response = requests.post(url, json=json)
     return response
-
-# TODO:
-# post_api_endpoint
